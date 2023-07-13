2e4e5c898407dd01e9ab427df4740c0d7c5b9b94
anangfath <anangmuhammad245@gmail.com>
2023-07-13 09:39:01 +0700

    fix update unit

--- resources/views/pages/administrator/unit/edit.blade.php
+++ resources/views/pages/administrator/unit/edit.blade.php
@@ -1,16 +1,55 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
-    <div class="container">
         <h1>Edit Unit</h1>
-        <form action="{{ route('administrator.unit.update', $unit->id) }}" method="POST">
-            @csrf
-            @method('PUT')
-            <div class="mb-3">
-                <label for="name" class="form-label">Name</label>
-                <input type="text" name="name" id="name" class="form-control" value="{{ $unit->name }}">
+            <div class="col-md-12 col-12">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <form class="form form-vertical" action="{{ route('administrator.unit.update', $unit->id) }}" method="POST">
+                                @csrf
+                                <div class="form-body">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="name">Name</label>
+                                                <div class="position-relative mb-3">
+                                                    <input type="text" class="form-control"
+                                                        placeholder="Unit Name" name="name" id="name" value="{{ $unit->name }}">
+                                                    <div class="form-control-icon">
+                                                        <i class="bi bi-house-door"></i>
+                                                    </div>
+                                                </div>
+                                                <label for="desc">Description</label>
+                                                <div class="position-relative mb-3">
+                                                    <input class="form-control"
+                                                    placeholder="Unit Description" name="desc" id="desc" value="{{ $unit->desc }}"></input>
+                                                    <div class="form-control-icon">
+                                                        <i class="bi bi-journal"></i>
+                                                    </div>
+                                                </div>
+                                                <label for="higher_domain">Sub Domain</label>
+                                                <div class="position-relative mb-3">
+                                                    <input type="text" class="form-control"
+                                                        placeholder="Unit Sub Domain" name="higher_domain" id="higher_domain" value="{{ $unit->higer_domain }}">
+                                                    <div class="form-control-icon">
+                                                        <i class="bi bi-globe"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                        <div class="col-12">
+                                        </div>
+                                        <div class="col-12 d-flex justify-content-start">
+                                            <button type="submit" class="btn btn-primary me-1 mb-1">Submit</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <button type="submit" class="btn btn-primary">Update</button>
-        </form>
-    </div>
+
 @endsection
